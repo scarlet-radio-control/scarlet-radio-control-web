46119a9d2f99a5d98aee062330d495ed528be9b3
Raul Hidalgo <deinok@deinok.com>
2026-01-29 17:56:14 +0100

    .

--- src/ScarletRadioControl.Web/Program.cs
+++ src/ScarletRadioControl.Web/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace ReactApp1.Server;
+namespace ScarletRadioControl.Web;
 
 public class Program
 {
