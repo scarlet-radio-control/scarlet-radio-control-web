b0fbd7feba0812b3811b90ceeb71284677203d78
Raul Hidalgo <deinok@deinok.com>
2026-01-30 19:51:30 +0100

    .

--- src/ScarletRadioControl.Web.Frontend/eslint.config.js
+++ src/ScarletRadioControl.Web.Frontend/eslint.config.js
@@ -8,16 +8,16 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+	files: ['**/*.{ts,tsx}'],
+	extends: [
+	  js.configs.recommended,
+	  tseslint.configs.recommended,
+	  reactHooks.configs.flat.recommended,
+	  reactRefresh.configs.vite,
+	],
+	languageOptions: {
+	  ecmaVersion: 2020,
+	  globals: globals.browser,
+	},
   },
 ])
--- src/ScarletRadioControl.Web.Frontend/vite.config.ts
+++ src/ScarletRadioControl.Web.Frontend/vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler']
-      }
-    })
+	react({
+	  babel: {
+		plugins: ['babel-plugin-react-compiler']
+	  }
+	})
   ],
 })
