bf65fc8b5f7ca5f5e11d982f8118ff61f5abf00f
Raul Hidalgo <deinok@deinok.com>
2026-02-02 19:31:52 +0100

    .

--- src/ScarletRadioControl.Web.Frontend/src/main.tsx
+++ src/ScarletRadioControl.Web.Frontend/src/main.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
+const htmlElement = document.getElementById('root')!;
+const root = createRoot(htmlElement)
+root.render(
 	<StrictMode>
 		<BrowserRouter>
-			<App />
+			<Routes>
+				<Route path="/" element={<App />} />
+			</Routes>
 		</BrowserRouter>
   </StrictMode>,
 )
