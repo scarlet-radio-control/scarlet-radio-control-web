820fc0645782643a62a420c8be4d56fb5257ed68
Raul Hidalgo <deinok@deinok.com>
2026-02-09 21:22:49 +0100

    .

--- src/ScarletRadioControl.Web.Frontend/src/App.tsx
+++ src/ScarletRadioControl.Web.Frontend/src/App.tsx
@@ -3,10 +3,10 @@ import Index from './pages/Index.tsx'
 import GamepadTest from './pages/GamepadTest.tsx'
 
 export default function App() {
-  return (
-	<Routes>
-		<Route path="/" element={<Index />} />
-		<Route path="/gamepad-test" element={<GamepadTest />} />
-	</Routes>
-  )
+	return (
+		<Routes>
+			<Route path="/" element={<Index />} />
+			<Route path="/gamepad-test" element={<GamepadTest />} />
+		</Routes>
+	)
 }
