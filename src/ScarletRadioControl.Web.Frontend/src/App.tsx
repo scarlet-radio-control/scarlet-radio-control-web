1fe113eb67bb0aaf39281f0278fb949cd3fd1774
Raul Hidalgo <deinok@deinok.com>
2026-02-10 16:41:58 +0100

    .

--- src/ScarletRadioControl.Web.Frontend/src/App.tsx
+++ src/ScarletRadioControl.Web.Frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
-import Index from './pages/Index.tsx'
 import GamepadTest from './pages/GamepadTest.tsx'
+import Index from './pages/Index.tsx'
+import WebRtcTest from './pages/WebRtcTest.tsx'
 
 export default function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Index />} />
 			<Route path="/gamepad-test" element={<GamepadTest />} />
+			<Route path="/web-rtc-test" element={<WebRtcTest />} />
 		</Routes>
 	)
 }
---
+++ src/ScarletRadioControl.Web.Frontend/src/pages/WebRtcTest.tsx
@@ -0,0 +1,7 @@
+export default function GamepadTest() {
+    return (
+        <div>
+            <h1>WebRtc Test Page</h1>
+        </div>
+    );
+}
