261ca959440713993c33b20aee268428e922da18
Raul Hidalgo <deinok@deinok.com>
2026-02-10 17:19:26 +0100

    .

--- src/ScarletRadioControl.Web.Frontend/vite.config.ts
+++ src/ScarletRadioControl.Web.Frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl  from '@vitejs/plugin-basic-ssl'
 import tailcindcss from '@tailwindcss/vite'
 
 export default defineConfig({
 	plugins: [
+		basicSsl(),
 		react({
 			babel: {
 				plugins: ['babel-plugin-react-compiler']
