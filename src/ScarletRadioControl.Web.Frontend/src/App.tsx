613430b496057f6217bf1d47e279d169b7bd950f
Raul Hidalgo <deinok@deinok.com>
2026-02-12 18:56:29 +0100

    .

--- src/ScarletRadioControl.Web.Frontend/src/App.tsx
+++ src/ScarletRadioControl.Web.Frontend/src/App.tsx
@@ -4,6 +4,8 @@ import Index from './pages/Index.tsx'
 import Basic from './pages/WebRtcTests/Basic.tsx'
 import TabCallee from './pages/WebRtcTests/TabCallee.tsx'
 import TabCaller from './pages/WebRtcTests/TabCaller.tsx'
+import VideoReceiver from './pages/WebRtcTests/VideoReceiver.tsx'
+import VideoSender from './pages/WebRtcTests/VideoSender.tsx'
 
 export default function App() {
 	return (
@@ -13,6 +15,8 @@ export default function App() {
 			<Route path="/web-rtc-tests/basic" element={<Basic />} />
 			<Route path="/web-rtc-tests/tab-callee" element={<TabCallee />} />
 			<Route path="/web-rtc-tests/tab-caller" element={<TabCaller />} />
+			<Route path="/web-rtc-tests/video-receiver" element={<VideoReceiver />} />
+			<Route path="/web-rtc-tests/video-sender" element={<VideoSender />} />
 		</Routes>
 	)
 }
