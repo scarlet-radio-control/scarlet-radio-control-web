c7c0a9960cb39ae3c3d2062802c1c1f060b1f0e7
Raul Hidalgo <deinok@deinok.com>
2026-02-12 18:04:47 +0100

    .

--- src/ScarletRadioControl.Web.Frontend/src/App.tsx
+++ src/ScarletRadioControl.Web.Frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import GamepadTest from './pages/GamepadTest.tsx'
 import Index from './pages/Index.tsx'
-import WebRtcTest from './pages/WebRtcTest.tsx'
+import Basic from './pages/WebRtcTests/Basic.tsx'
 
 export default function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Index />} />
 			<Route path="/gamepad-test" element={<GamepadTest />} />
-			<Route path="/web-rtc-test" element={<WebRtcTest />} />
+			<Route path="/web-rtc-tests/basic" element={<Basic />} />
 		</Routes>
 	)
 }
