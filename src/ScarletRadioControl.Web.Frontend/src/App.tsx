eab271c9eb96c67eef85f325af2dde7d7bc494e5
Raul Hidalgo <deinok@deinok.com>
2026-02-12 18:08:21 +0100

    .

--- src/ScarletRadioControl.Web.Frontend/src/App.tsx
+++ src/ScarletRadioControl.Web.Frontend/src/App.tsx
@@ -2,8 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import GamepadTest from './pages/GamepadTest.tsx'
 import Index from './pages/Index.tsx'
 import Basic from './pages/WebRtcTests/Basic.tsx'
-import Callee from './pages/WebRtcTests/Callee.tsx'
-import Caller from './pages/WebRtcTests/Caller.tsx'
+import TabCallee from './pages/WebRtcTests/TabCallee.tsx'
+import TabCaller from './pages/WebRtcTests/TabCaller.tsx'
 
 export default function App() {
 	return (
@@ -11,8 +11,8 @@ export default function App() {
 			<Route path="/" element={<Index />} />
 			<Route path="/gamepad-test" element={<GamepadTest />} />
 			<Route path="/web-rtc-tests/basic" element={<Basic />} />
-			<Route path="/web-rtc-tests/callee" element={<Callee />} />
-			<Route path="/web-rtc-tests/caller" element={<Caller />} />
+			<Route path="/web-rtc-tests/tab-callee" element={<TabCallee />} />
+			<Route path="/web-rtc-tests/tab-caller" element={<TabCaller />} />
 		</Routes>
 	)
 }
