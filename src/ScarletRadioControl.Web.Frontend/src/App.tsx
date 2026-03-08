fdecabf5190cdcae2dd4806859eeed0e82bce7b9
Raul Hidalgo <deinok@deinok.com>
2026-03-08 13:34:47 +0100

    .

--- src/ScarletRadioControl.Web.Frontend/src/App.tsx
+++ src/ScarletRadioControl.Web.Frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom"
 import GamepadTest from "./pages/GamepadTest.tsx"
-import {HubConnectionProvider} from "./context/HubConnectionContext.tsx"
 import Index from "./pages/Index.tsx"
 import Basic from "./pages/WebRtcTests/Basic.tsx"
 import Control from "./pages/device/Control.tsx"
@@ -13,20 +12,18 @@ import ControlTest from "./pages/device/ControlTest.tsx"
 
 export default function App() {
 	return (
-		<HubConnectionProvider>
-			<Routes>
-				<Route path="/" element={<Index />} />
-				<Route path="/device/:deviceId/control" element={<Control />} />
-				<Route path="/device/:deviceId/control-test" element={<ControlTest />} />
-				<Route path="/gamepad-test" element={<GamepadTest />} />
-				<Route path="/web-rtc-tests/basic" element={<Basic />} />
-				<Route path="/web-rtc-tests/tab-callee" element={<TabCallee />} />
-				<Route path="/web-rtc-tests/tab-caller" element={<TabCaller />} />
-				<Route path="/web-rtc-tests/signalr-video-receiver" element={<SignalRVideoReceiver roomId="test-room" />} />
-				<Route path="/web-rtc-tests/signalr-video-sender" element={<SignalRVideoSender roomId="test-room" />} />
-				<Route path="/web-rtc-tests/video-receiver" element={<VideoReceiver />} />
-				<Route path="*" element={<h1>404 Not Found</h1>} />
-			</Routes>
-		</HubConnectionProvider>
+		<Routes>
+			<Route path="/" element={<Index />} />
+			<Route path="/device/:deviceId/control" element={<Control />} />
+			<Route path="/device/:deviceId/control-test" element={<ControlTest />} />
+			<Route path="/gamepad-test" element={<GamepadTest />} />
+			<Route path="/web-rtc-tests/basic" element={<Basic />} />
+			<Route path="/web-rtc-tests/tab-callee" element={<TabCallee />} />
+			<Route path="/web-rtc-tests/tab-caller" element={<TabCaller />} />
+			<Route path="/web-rtc-tests/signalr-video-receiver" element={<SignalRVideoReceiver roomId="test-room" />} />
+			<Route path="/web-rtc-tests/signalr-video-sender" element={<SignalRVideoSender roomId="test-room" />} />
+			<Route path="/web-rtc-tests/video-receiver" element={<VideoReceiver />} />
+			<Route path="*" element={<h1>404 Not Found</h1>} />
+		</Routes>
 	)
 }
