14320a9ae007ca95d264f5e4bce453516c9217a7
Raul Hidalgo <deinok@deinok.com>
2026-05-10 18:30:29 +0200

    .

--- src/ScarletRadioControl.Web.Frontend/src/hooks/useApiClient.tsx
+++ src/ScarletRadioControl.Web.Frontend/src/hooks/useApiClient.tsx
@@ -1,17 +1,18 @@
 import { AnonymousAuthenticationProvider,  } from "@microsoft/kiota-abstractions";
 import { FetchRequestAdapter } from "@microsoft/kiota-http-fetchlibrary";
-import { useRef, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { createApiClient, type ApiClient } from "../kiota/output/apiClient";
 
 export default function useApiClient() {
-    const apiClientRefObject = useRef<ApiClient>(undefined);
+    const [apiClient, setApiClient] = useState<ApiClient | undefined>(undefined);
 
     useEffect(() => {    
         const anonymousAuthenticationProvider = new AnonymousAuthenticationProvider();
         const fetchRequestAdapter = new FetchRequestAdapter(anonymousAuthenticationProvider);
-        apiClientRefObject.current = createApiClient(fetchRequestAdapter);
+        const newApiClient = createApiClient(fetchRequestAdapter);
+        setApiClient(newApiClient);
 
         return () => { };
     }, []);
-    return apiClientRefObject.current;
+    return apiClient;
 }
