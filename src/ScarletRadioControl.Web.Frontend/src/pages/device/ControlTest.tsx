5f9f6c2fb63a9abe69df8bd9161dd78954f67fc2
Raul Hidalgo <deinok@deinok.com>
2026-07-19 11:23:12 +0200

    .

--- src/ScarletRadioControl.Web.Frontend/src/pages/device/ControlTest.tsx
+++ src/ScarletRadioControl.Web.Frontend/src/pages/device/ControlTest.tsx
@@ -58,7 +58,7 @@ export default function ControlTest() {
 		});
 
 		return () => {};
-	}, [apiClient]);
+	}, [apiClient, heartbeatStatus]);
 
 	useEffect(() => {
 		if (!connected || !deviceId || !hubConnection || !rtcPeerConnection) { return; }
